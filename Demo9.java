c3061f208b1ebbff6f3ca17935dcf07940dda3b3
ManaliAundhkar <56449889+ManaliAundhkar@users.noreply.github.com>
2020-03-21 17:28:55 +0530

    Update Demo9.java

--- Demo9.java
+++ Demo9.java
@@ -1,9 +1,9 @@
 //Write Java program which accept number of rows and number of columns from user and display below pattern.
 //Input : iRow = 3 iCol = 4
 //Output : 1 1 1 1
-//		   2 2 2 2
-//		   3 3 3 3
-//		   4 4 4 4
+//	   2 2 2 2
+//	   3 3 3 3
+//	   4 4 4 4
 
 import java.util.*;
 
